9751942af438b1dcf65e92cfad8f932814e81da2
Rowandjj <rjchuyi@gmail.com>
2017-03-03 16:44:13 +0800

    update

--- run-circle-ci.sh
+++
@@ -1,3 +0,0 @@
-# !/bin/sh -eu
-cd playground
-./gradlew clean build test :weex_analyzer:testDebugUnitTestCoverage -Dorg.gradle.daemon=true -Dorg.gradle.parallel=true -Dorg.gradle.jvmargs="-Xmx512m -XX:+HeapDumpOnOutOfMemoryError" -Dfile.encoding=UTF-8
