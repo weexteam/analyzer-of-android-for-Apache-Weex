45540bc75911e6e42492049f0e7798190b287385
Rowandjj <rjchuyi@gmail.com>
2017-03-03 14:10:53 +0800

    update ci.sh

--- weex_analyzer/run-circle-ci.sh
+++ weex_analyzer/run-circle-ci.sh
@@ -1,3 +1,2 @@
 # !/bin/sh -eu
-cd weex_analyzer
 ./gradlew clean build test testDebugUnitTestCoverage -Dorg.gradle.daemon=true -Dorg.gradle.parallel=true -Dorg.gradle.jvmargs="-Xmx512m -XX:+HeapDumpOnOutOfMemoryError" -Dfile.encoding=UTF-8
