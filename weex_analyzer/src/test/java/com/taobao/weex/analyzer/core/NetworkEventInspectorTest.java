f7805563c69b36955e5cc895a09a0e8c1dace493
Rowandjj <rjchuyi@gmail.com>
2017-03-06 10:05:43 +0800

    update

---
+++ weex_analyzer/src/test/java/com/taobao/weex/analyzer/core/NetworkEventInspectorTest.java
@@ -0,0 +1,33 @@
+package com.taobao.weex.analyzer.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Description:
+ *
+ * Created by rowandjj(chuyi)<br/>
+ */
+public class NetworkEventInspectorTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void createInstance() throws Exception {
+
+    }
+
+    @Test
+    public void destroy() throws Exception {
+
+    }
+
+}
