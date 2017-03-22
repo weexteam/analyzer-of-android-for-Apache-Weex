c0e0dc523903dde3b0fa478a02336bb40180a5c1
Rowandjj <rjchuyi@gmail.com>
2017-03-22 17:10:20 +0800

    use online request url by default

--- weex_analyzer/src/main/java/com/taobao/weex/analyzer/core/reporter/MDSDataReporterFactory.java
+++ weex_analyzer/src/main/java/com/taobao/weex/analyzer/core/reporter/MDSDataReporterFactory.java
@@ -14,12 +14,12 @@ public class MDSDataReporterFactory {
     private MDSDataReporterFactory(){}
 
     private static final String REQUEST_URL_PRE = "http://pre.mds.alibaba-inc.com/api/debug/weexAnalyzer/%s/logs";
-    private static final String REQUEST_URL_ONLINE = "";//TODO
+    private static final String REQUEST_URL_ONLINE = "http://mds.alibaba-inc.com/api/debug/weexAnalyzer/%s/logs";
 
     private static final String MDS = "mds";
 
     @NonNull
     public static <T> IDataReporter<T> create(@NonNull String from,@NonNull String deviceId) {
-        return new HttpDataReporter<>(String.format(Locale.CHINA,REQUEST_URL_PRE,deviceId),MDS.equals(from));
+        return new HttpDataReporter<>(String.format(Locale.CHINA,REQUEST_URL_ONLINE,deviceId),MDS.equals(from));
     }
 }
